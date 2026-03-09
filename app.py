77571d45658e14e5a1b3cdb427c1bfabd0f5ecc8
Nisha Robin <nisha@Nishas-Air.Dlink>
2026-03-09 13:27:04 +0530

    Fix fleet loading

--- app.py
+++ app.py
@@ -260,8 +260,3 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port, debug=True)
 
 import os
-
-if __name__ == '__main__':
-    # Get the port from the environment, default to 5000 if not set
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
